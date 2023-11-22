7369c957c2a0ea479de714f9af2daf613d42d664
amarianjel <abrm.enginner@gmail.com>
2023-11-22 11:04:02 -0300

    Navegar a la segunda pantalla

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:push_app/presentation/screens/details_screen.dart';
 import 'package:push_app/presentation/screens/home_screen.dart';
 
 
@@ -12,10 +13,10 @@ final appRouter = GoRouter(
       builder: (context, state) => const HomeScreen(),
     ),
 
-    // GoRoute(
-    //   path: '/push-details/:messageId',
-    //   builder: (context, state) => DetailsScreen( pushMessageId: state.params['messageId'] ?? '', ),
-    // ),
+    GoRoute(
+      path: '/push-details/:messageId',
+      builder: (context, state) => DetailsScreen( pushMessageId: state.pathParameters['messageId'] ?? '', ),
+    ),
 
   ]
   
